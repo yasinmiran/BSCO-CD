cd695ab30dcd01bcd5a6c438282f59b195037515
Yasin <wytm97@protonmail.com>
2023-09-27 16:26:13 +0100

    add message printer code

--- src/main/java/dev/yasint/Main.java
+++ src/main/java/dev/yasint/Main.java
@@ -8,10 +8,17 @@ public class Main {
         MyOtherTask myOtherTask = new MyOtherTask();
 
         Thread myTask_Thread = new Thread(myTask);
-        myTask_Thread.setDaemon(false);
+        myTask_Thread.setDaemon(false); // This will wait
+        // myTask_Thread.setDaemon(true); // This will not wait
 
         myTask_Thread.start();
         myOtherTask.start();
+
+        Thread messagePrinterThread = new Thread(new MessagePrinter(
+                "My Message", 10
+        ));
+
+        messagePrinterThread.start();
 
     }
 
